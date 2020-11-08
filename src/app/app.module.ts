11186df65f7260f399e46723b2a0f9e558112d2c
pasierbkarol <pasierbkarol@gmail.com>
2020-11-08 16:18:46 +0000

    Create Welcome info component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeInfoComponent } from './welcome-info/welcome-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeInfoComponent
   ],
   imports: [
     BrowserModule,
