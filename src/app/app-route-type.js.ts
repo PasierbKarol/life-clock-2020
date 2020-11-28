2e0b6886a96efe40bcfb369cfe75417e8fb614f4
pasierbkarol <pasierbkarol@gmail.com>
2020-11-28 15:36:17 +0000

    Add route types and installed immutable

---
+++ src/app/app-route-type.js.ts
@@ -0,0 +1,19 @@
+import { Map as ImmutableMap } from 'immutable';
+
+export const APP_ROUTE_PREFIX = 'app';
+
+export enum AppRouteType {
+  APP_LOGIN = 'login',
+  APP_REGISTRATION = 'registration',
+  WELCOME = 'welcome',
+  CLOCK = 'clock'
+}
+
+export const APP_ROUTE_TYPE_TITLE =
+  ImmutableMap<AppRouteType, string>
+  (new Map<AppRouteType, string>([
+    [AppRouteType.APP_LOGIN, 'Login'],
+    [AppRouteType.APP_REGISTRATION, 'Registration'],
+    [AppRouteType.WELCOME, 'Life Clock 2020 - Welcome'],
+    [AppRouteType.CLOCK, 'Life Cl`ock']
+  ]));
