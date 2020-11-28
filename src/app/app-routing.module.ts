00bc7f20cccad355f36207148d696b28fb789ee5
pasierbkarol <pasierbkarol@gmail.com>
2020-11-28 15:40:34 +0000

    Update routing module@

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,34 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {APP_ROUTE_TYPE_TITLE, AppRouteType} from './app-route-type.js';
+import {WelcomeInfoComponent} from './welcome-info/welcome-info.component';
 
+export const APP_ROUTE_PREFIX = 'app';
+const DEFAULT_ROUTE = `${APP_ROUTE_PREFIX}/`;
 
-const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', redirectTo: DEFAULT_ROUTE, pathMatch: 'full', canActivate: [AuthorisationGuard]},
+  {path: AppRouteType.APP_LOGIN, component: LoginFormComponent, data: {title: APP_ROUTE_TYPE_TITLE.get(AppRouteType.APP_LOGIN)}},
+  {
+    path: AppRouteType.APP_REGISTRATION,
+    component: RegistrationFormComponent,
+    data: {title: APP_ROUTE_TYPE_TITLE.get(AppRouteType.APP_REGISTRATION)}
+  },
 
+  {
+    path: AppRouteType.WELCOME,
+    component: WelcomeInfoComponent,
+    data: {title: APP_ROUTE_TYPE_TITLE.get(AppRouteType.WELCOME)},
+    canActivate: [AuthorisationGuard]
+  },
+  {
+    path: AppRouteType.CLOCK,
+    component: LevelsInCalendarComponent,
+    data: {title: APP_ROUTE_TYPE_TITLE.get(AppRouteType.CLOCK)},
+    canActivate: [AuthorisationGuard]
+  },
+  {path: '**', redirectTo: DEFAULT_ROUTE}
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
