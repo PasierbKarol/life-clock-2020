88a760da73883689e023b38b28a805d9428ff3cf
pasierbkarol <pasierbkarol@gmail.com>
2020-11-29 14:40:43 +0000

    Add ngx translate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomeInfoComponent } from './welcome-info/welcome-info.component';
 import { LifeClockComponent } from './life-clock/life-clock.component';
 import { GoalsBoxComponent } from './goals-box/goals-box.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [
@@ -16,9 +16,11 @@ import { GoalsBoxComponent } from './goals-box/goals-box.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TranslateModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
