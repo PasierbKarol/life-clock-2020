1851d259eb83356e71465e75b2ed95aab13b1c43
pasierbkarol <pasierbkarol@gmail.com>
2020-12-03 16:05:41 +0000

    Do some css and html cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppComponent } from './app.component';
 import { WelcomeInfoComponent } from './welcome-info/welcome-info.component';
 import { LifeClockComponent } from './life-clock/life-clock.component';
 import { GoalsBoxComponent } from './goals-box/goals-box.component';
-import { TranslateModule } from '@ngx-translate/core';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 
 @NgModule({
@@ -19,7 +18,6 @@ import { NavigationBarComponent } from './navigation-bar/navigation-bar.componen
   imports: [
     BrowserModule,
     AppRoutingModule,
-    TranslateModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
