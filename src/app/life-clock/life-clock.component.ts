4d094da4cbeeefc56ef3cd468fbac33e62464bf5
pasierbkarol <pasierbkarol@gmail.com>
2020-12-07 20:24:55 +0000

    Add grid and functionality to goals list button

--- src/app/life-clock/life-clock.component.ts
+++ src/app/life-clock/life-clock.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { ContentConstants } from './content-constants';
 
 @Component({
@@ -8,11 +8,14 @@ import { ContentConstants } from './content-constants';
 })
 export class LifeClockComponent implements OnInit {
 
-  public goals: any = ContentConstants.lifeClockEN.goalBlocks;
-  public sections: number = this.goals.length;
+  @ViewChild('draggableGoalsList', { static: false }) public draggableGoalsList: ElementRef;
+  public goalsTexts: any = ContentConstants.lifeClockEN.goalBlocks;
+  public sections: number = this.goalsTexts.length;
   public currentlyVisibleSection: number = 0;
+  public goals: string[];
+  public areGoalsSubmitted: boolean = false;
 
-  public constructor() {
+  public constructor(private renderer: Renderer2) {
   }
 
   public ngOnInit() {
@@ -21,5 +24,24 @@ export class LifeClockComponent implements OnInit {
   public sectionCompleted(id: number): void {
     console.log(id, 'section completed');
     this.currentlyVisibleSection = id > this.currentlyVisibleSection ? id : this.currentlyVisibleSection;
+  }
+
+  public listSubmitted(goalsFromView: any): void {
+    this.areGoalsSubmitted = true;
+    console.log(goalsFromView.split(/\n/g), 'value goals');
+    console.log(this.draggableGoalsList, 'el ref');
+    this.goals = goalsFromView.split(/\n/g);
+    this.composeDraggableGoals();
+
+  }
+
+  private composeDraggableGoals(): void {
+    for (const goal of this.goals) {
+      const dragGoal = this.renderer.createElement('div');
+      this.renderer.addClass(dragGoal, 'draggable-goal');
+      const text = this.renderer.createText(goal);
+      this.renderer.appendChild(dragGoal, text);
+      this.renderer.appendChild(this.draggableGoalsList.nativeElement, dragGoal);
+    }
   }
 }
