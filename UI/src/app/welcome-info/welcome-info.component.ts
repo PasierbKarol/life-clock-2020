1d0a898e9567028c4eb94a8d8f8f73080dab66df
Karol Pasierb <pasierbkarol@gmail.com>
2021-04-24 21:23:14 +0100

    Add button to start exercise from welcome page

--- UI/src/app/welcome-info/welcome-info.component.ts
+++ UI/src/app/welcome-info/welcome-info.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AppConstants } from 'src/app/app-constants';
+import { AppRouteType } from 'src/app/app-route-type.js';
 
 @Component({
   selector: 'app-welcome-info',
   templateUrl: './welcome-info.component.html',
   styleUrls: ['./welcome-info.component.scss']
 })
-export class WelcomeInfoComponent implements OnInit {
+export class WelcomeInfoComponent {
 
   private descriptions = AppConstants.getDescriptions('PL');
   public title = this.descriptions.introduction_title;
@@ -14,10 +16,10 @@ export class WelcomeInfoComponent implements OnInit {
   public descriptionMiddle = this.descriptions.introduction_middle;
   public descriptionFinal = this.descriptions.introduction_final;
 
-  constructor() {
+  constructor(private router: Router) {
   }
 
-  ngOnInit() {
+  public startLifeClock() {
+    this.router.navigate([AppRouteType.CLOCK]);
   }
-
 }
