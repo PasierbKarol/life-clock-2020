679fea810927596ebdc8865e910255031d018e8f
Karol Pasierb <pasierbkarol@gmail.com>
2021-04-25 19:12:19 +0100

    Add snackbar for handling success and error responses

--- UI/src/app/completed-goals/completed-goals.component.ts
+++ UI/src/app/completed-goals/completed-goals.component.ts
@@ -1,6 +1,8 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, Input, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
-import {saveAs} from 'file-saver';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { saveAs } from 'file-saver';
 import { PersonalDetailsModel } from 'src/app/models/personal-details.model';
 import { ResponseModel } from 'src/app/models/response.model';
 import { BackendService } from 'src/app/services/backend.service';
@@ -18,7 +20,8 @@ export class CompletedGoalsComponent implements OnInit {
 
   public constructor(
     private backend: BackendService,
-    private goalsProvider: GoalsProviderService
+    private goalsProvider: GoalsProviderService,
+    private snackBar: MatSnackBar
   ) {
   }
 
@@ -30,21 +33,40 @@ export class CompletedGoalsComponent implements OnInit {
 
   public sendByEmail(): void {
     const response: ResponseModel = new ResponseModel();
-      this.backend.sendGoalsByEmail(this.personalDetails).subscribe(r => {
+    this.backend.sendGoalsByEmail(this.personalDetails).subscribe(r => {
         response.httpCode = r.httpCode;
         response.responseJSON = r.responseJSON;
-      });
+        this.successfulOperation('The email was sent successfully!');
+      },
+      error => this.errorHandling(error, 'The email could not be sent! Error=')
+    );
   }
 
   public exportToPDF(): void {
-    this.backend.exportGoalsToPDF(this.personalDetails).subscribe(
+    this.backend.exportGoalsToPDF(this.personalDetails.submittedGoals).subscribe(
       response => {
-        saveAs(response, `Life-Clock-Goals_${this.personalDetails.name}.pdf`)},
-        error => this.errorHandling(error));
+        saveAs(response, `Life-Clock-Goals.pdf`);
+        this.successfulOperation('The PDF was successfully created!');
+      },
+      error => this.errorHandling(error, 'The PDF could not be exported! Error='));
   }
 
-  private errorHandling(error: String) {
-    console.log(error, 'Inside error handling')
+  private errorHandling(error: HttpErrorResponse, message: string): void {
+    const emailSentSnackBar = this.snackBar.open(`${message}${error.message}`, 'Close', {
+      duration: 3500,
+      horizontalPosition: 'center',
+      verticalPosition: 'bottom'
+    });
+    emailSentSnackBar.onAction().subscribe(() => emailSentSnackBar.dismiss());
+  }
+
+  private successfulOperation(message: string): void {
+    const successfulSnackBar = this.snackBar.open(message, 'Close', {
+      duration: 3500,
+      horizontalPosition: 'center',
+      verticalPosition: 'bottom'
+    });
+    successfulSnackBar.onAction().subscribe(() => successfulSnackBar.dismiss());
   }
 
   public emailInput = new FormControl('', [Validators.required, Validators.email]);
--- UI/src/app/services/backend.service.ts
+++ UI/src/app/services/backend.service.ts
@@ -1,8 +1,9 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { map, tap } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 import { ExportToEmailRequestModel } from 'src/app/models/export-to-email-request.model';
+import { LifeGoalModel } from 'src/app/models/life-goal.model';
 import { PersonalDetailsModel } from 'src/app/models/personal-details.model';
 import { ResponseModel } from 'src/app/models/response.model';
 import { environment } from 'src/environments/environment';
@@ -12,12 +13,6 @@ import { environment } from 'src/environments/environment';
 })
 export class BackendService {
   private SERVICE_PATH: string = environment.serverEndpoint;
-
-  public corsHeaders = new HttpHeaders({
-    'Content-Type': 'application/json',
-    'Accept': 'application/json',
-    'Access-Control-Allow-Origin': 'http://localhost:8080/'
-  });
 
   constructor(private http: HttpClient) {
   }
@@ -32,22 +27,11 @@ export class BackendService {
     };
 
     return this.http.post<ResponseModel>(`${this.SERVICE_PATH}/send-goals-by-email`, data).pipe(
-      tap( // Log the result or error
-        data => console.log(data, 'returned from the server'),
-        error => console.log(error, 'error from the server')
-      )
+      map(r => r)
     );
   }
 
-  public exportGoalsToPDF(details: PersonalDetailsModel): Observable<Blob> {
-    const data: ExportToEmailRequestModel = {
-      personalDetails: {
-        name: details.name,
-        surname: details.surname,
-        email: details.email
-      }, goals: details.submittedGoals
-    };
-
+  public exportGoalsToPDF(dataToExport: LifeGoalModel[]): Observable<Blob> {
     const HTTPOptions = {
       headers: new HttpHeaders({
         'Accept': 'application/pdf'
@@ -55,7 +39,7 @@ export class BackendService {
       'responseType': 'blob' as 'json'
     };
 
-    return this.http.post<Blob>(`${this.SERVICE_PATH}/export-goals-by-pdf`, data, HTTPOptions).pipe(
+    return this.http.post<Blob>(`${this.SERVICE_PATH}/export-goals-by-pdf`, dataToExport, HTTPOptions).pipe(
       map(response => response)
     );
   }
